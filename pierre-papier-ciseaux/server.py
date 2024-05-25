59fb5048b0ce79570d67d0619408fddddb55105d
ChoukriSaid <101629458+ChoukriSaid@users.noreply.github.com>
2024-05-25 15:58:44 +0000

    Add files via upload

---
+++ pierre-papier-ciseaux/server.py
@@ -0,0 +1,76 @@
+import socket
+from _thread import *
+import pickle
+from game import Game
+
+server = "172.20.10.3"
+port = 5555
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+try:
+    s.bind((server, port))
+except socket.error as e:
+    str(e)
+
+s.listen(2)
+print("En attente de connection, serveur intialisé ..")
+
+connected = set()
+games = {}
+idCount = 0
+
+
+def threaded_client(conn, p, gameId):
+    global idCount
+    conn.send(str.encode(str(p)))
+
+    reply = ""
+    while True:
+        try:
+            data = conn.recv(4096).decode()
+
+            if gameId in games:
+                game = games[gameId]
+
+                if not data:
+                    break
+                else:
+                    if data == "reset":
+                        game.resetWent()
+                    elif data != "get":
+                        game.play(p, data)
+
+                    conn.sendall(pickle.dumps(game))
+            else:
+                break
+        except:
+            break
+
+    print("Connection échoué")
+    try:
+        del games[gameId]
+        print("Fermeture", gameId)
+    except:
+        pass
+    idCount -= 1
+    conn.close()
+
+
+
+while True:
+    conn, addr = s.accept()
+    print("Connecté à :", addr)
+
+    idCount += 1
+    p = 0
+    gameId = (idCount - 1)//2
+    if idCount % 2 == 1:
+        games[gameId] = Game(gameId)
+        print("Creation d'une partie ..")
+    else:
+        games[gameId].ready = True
+        p = 1
+
+
+    start_new_thread(threaded_client, (conn, p, gameId))
